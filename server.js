49d6f2016c49c8c51c0972eea3eb1c28b2902cd1
Viharika <viharika807@gmail.com>
2023-05-25 11:09:38 +0530

    step-3 completed

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const endpoints = [
   { path: '/i/google', description: 'Link to Google', response: 'HTML anchor tag' },
   { path: '/i/github', description: 'Link to Github', response: 'HTML anchor tag' },
   { path: '/i', description: 'List all the endpoints that are available', response: 'Endpoints List' },
+  { path: '/r/google', description: 'Redirect to Google', response: 'Redirect to https://google.com' },
+  { path: '/r/github', description: 'Redirect to Github', response: 'Redirect to https://github.com' },
+  { path: '/r', description: 'Information about redirection endpoint', response: 'Message about how to use /r/google and /r/github' },
 ];
 
 app.get('/', (req, res) => {
@@ -26,6 +29,20 @@ app.get('/i/google', (req, res) => {
 
 app.get('/i/github', (req, res) => {
   res.send('<a href="https://github.com/">Go to Github</a>');
+})
+
+app.get('/r', (req, res) => {
+  const message = 'To use the redirection endpoint, make a GET request to /r/google and /r/github.';
+  res.send(message);
+});
+
+app.get('/r/google', (req, res) => {
+  res.writeHead(302, { Location: 'https://www.google.com' });
+  res.end();
+})
+
+app.get('/r/github', (req, res) => {
+  res.redirect('https://github.com/');
 })
 
 app.listen(5005, () => {
