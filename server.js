870a376e8ddab14d8f0e52bfeb1a6cd326846919
Viharika <viharika807@gmail.com>
2023-05-25 11:08:12 +0530

    step-2 completed

--- server.js
+++ server.js
@@ -5,8 +5,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+const endpoints = [
+  { path: '/', description: 'Home page', response: 'Rendered HTML' },
+  { path: '/i/google', description: 'Link to Google', response: 'HTML anchor tag' },
+  { path: '/i/github', description: 'Link to Github', response: 'HTML anchor tag' },
+  { path: '/i', description: 'List all the endpoints that are available', response: 'Endpoints List' },
+];
+
 app.get('/', (req, res) => {
   res.render('index');
+})
+
+app.get('/i', (req, res) => {
+  res.json(endpoints);
+}); 
+
+app.get('/i/google', (req, res) => {
+  res.send('<a href="https://www.google.com">Go to Google</a>');
+})
+
+app.get('/i/github', (req, res) => {
+  res.send('<a href="https://github.com/">Go to Github</a>');
 })
 
 app.listen(5005, () => {
