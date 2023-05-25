537045c31cf2e005604027d683e39fb29bbe87df
Viharika <viharika807@gmail.com>
2023-05-25 11:15:37 +0530

    step-4 completed

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+const mappings = {}; 
+
 const endpoints = [
   { path: '/', description: 'Home page', response: 'Rendered HTML' },
   { path: '/i/google', description: 'Link to Google', response: 'HTML anchor tag' },
@@ -13,6 +15,8 @@ const endpoints = [
   { path: '/r/google', description: 'Redirect to Google', response: 'Redirect to https://google.com' },
   { path: '/r/github', description: 'Redirect to Github', response: 'Redirect to https://github.com' },
   { path: '/r', description: 'Information about redirection endpoint', response: 'Message about how to use /r/google and /r/github' },
+  { path: '/map', description: 'Save URL to alias mapping', response: 'Save a mapping between a URL and an alias' },
+  { path: '/mappings', description: 'Display alias-to-URL mappings', response: 'List of alias-to-URL mappings' },
 ];
 
 app.get('/', (req, res) => {
@@ -44,6 +48,19 @@ app.get('/r/google', (req, res) => {
 app.get('/r/github', (req, res) => {
   res.redirect('https://github.com/');
 })
+
+app.post('/map', (req, res) => {
+  const { url, alias } = req.body;
+  if (!url || !alias) {
+    return res.status(400).send('Both URL and alias are required.');
+  }
+  mappings[alias] = url;
+  res.send('Mapping saved successfully.');
+});
+
+app.get('/mappings', (req, res) => {
+  res.json(mappings);
+});
 
 app.listen(5005, () => {
   console.log('Server is running at port 5005');
